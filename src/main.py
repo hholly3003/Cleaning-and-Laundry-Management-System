e1b524e4d1c2f98a237323c1bde2133f8e559038
Holly <h.holly3003@gmail.com>
2021-02-24 23:00:51 +1100

    added factory pattern

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 #Load environment variable
 from dotenv import load_dotenv
 load_dotenv()
 
-#Flask app creation
-from flask import Flask
-app = Flask(__name__)
 
-#Load configuration from settings.py
-app.config.from_object("settings.app_config")
+db = SQLAlchemy()
+ma = Marshmallow()
 
-#Database connection
+def create_app():
+    #Flask app creation
+    app = Flask(__name__)
+    #Load configuration from settings.py
+    app.config.from_object("settings.app_config")
+
+    #Register flask extensions to the flask application
+    db.init_app(app)
+    ma.init_app(app)
+
+    #Register the commands blueprint into flask app
+    from commands import db_commands
+    app.register_blueprint(db_commands)
+
+    return app
