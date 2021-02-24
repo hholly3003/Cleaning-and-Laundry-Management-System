35715980eb2a46c484f7d99583e4b29698211689
Holly <h.holly3003@gmail.com>
2021-02-24 21:04:06 +1100

    added custom commands to create and drop database

---
+++ src/commands.py
@@ -0,0 +1,18 @@
+from main import db
+from flask import Blueprint
+
+db_commands = Blueprint("db-custom", __name__)
+
+@db_commands.cli.command("create")
+def create_db():
+    db.create_all()
+    print("Tables created")
+
+@db_commands.cli.command("drop")
+def drop_db():
+    db.drop_all()
+    print("Tables deleted")
+
+@db_commands.cli.command("seed")
+def seed_db():
+    pass
