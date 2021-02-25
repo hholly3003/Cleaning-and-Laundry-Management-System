9e675015cacd2f10d9ec1bef8e776a0c2835b587
Holly <h.holly3003@gmail.com>
2021-02-25 14:13:20 +1100

    added bcrypt for password hashing in user model

--- src/models/User.py
+++ src/models/User.py
@@ -1,4 +1,4 @@
-from main import db
+from main import db, bcrypt
 
 class User(db.Model):
     __tablename__ = "users"
@@ -6,6 +6,14 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
+
+    def hash_password(self, password):
+        """Hashed the password"""
+        self.password = bcrypt.generate_password_hash(password).decode("utf-8")
+    
+    def check_password(self, password):
+        """Check the password hash"""
+        return bcrypt.check_password_hash(self.password, password)
 
     def __repr__(self):
         return f"User: {self.email}"
